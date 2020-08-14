c2d2dc9120d464b1c64839200ca0f78d964631a3
HossamMuhammedOmar <hossamomar9876@gmail.com>
2020-08-14 14:16:31 +0200

    addimg route page

--- app.js
+++ app.js
@@ -5,16 +5,28 @@ const url = require('url');
 ///////////////////////////////
 // SERVER
 
+const tempOV = fs.readFileSync(`${__dirname}/template-overview.html`, 'utf-8');
+const tempCD = fs.readFileSync(`${__dirname}/template-card.html`, 'utf-8');
+const tempPR = fs.readFileSync(`${__dirname}/template-product.html`, 'utf-8');
 const data = fs.readFileSync(`${__dirname}/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
-  if (pathName === '/' || pathName === '/home') res.end('Welcome To Home Page');
+  // DISPLAY OVERVIEW PAGE
+  if (pathName === '/' || pathName === '/home') {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    res.end(tempOV);
+  }
+  // DISPLAY PRODUCT PAGE
   else if (pathName === '/about') res.end('Welcome To About Page');
+  // API
   else if (pathName === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
+    // NOT FOUND PAGE
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
